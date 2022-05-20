265189f45e4a67fe2abb5c571bd705c71f16b7c7
Mukunth-arya <mukunthnataraj@gmail.com>
2022-05-20 22:32:05 +0530

    stage2

--- service/service.go
+++ service/service.go
@@ -1,3 +1,48 @@
 package service
 
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
 
+	data "github.com/Mukunth-arya/coorinates/proto/coordinates"
+)
+
+const (
+	Map_Coordinates = "https://maps.googleapis.com/maps/api/geocode/json?key=MYKEY&address="
+)
+
+type Dataresult struct {
+	Result Results
+}
+type Results []Geometry
+type Geometry struct {
+	Geometry Location
+}
+type Location struct {
+	Location Coordinates
+}
+type Coordinates struct {
+	Latitute  string
+	Longitude string
+}
+
+func Coordinatedata(ctx context.Context, rr *data.CordinateRequest) (*data.CoordinateResponse, error) {
+
+	resp, err := http.Get(Map_Coordinates + rr.City)
+
+	if err != nil {
+		return &data.CoordinateResponse{}, errors.New("Please enter the proper city name")
+	}
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return &data.CoordinateResponse{}, errors.New("Unable to read the Value")
+	}
+	var datas Dataresult
+	json.Unmarshal(bytes, &datas)
+
+	return &data.CoordinateResponse{CoordinateData: datas.Result[0].Geometry.Location.Latitute}, errors.New("Sorry unable to process th request!!!!")
+
+}
